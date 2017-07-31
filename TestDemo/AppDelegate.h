8d33bdcacfa173cc1688f57eacb330d6c30c1b26
LWHONG <lw-hong@hotmail.com>
2017-07-31 09:49:32 +0800

    Add files via upload
    add files

---
+++ TestDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TestDemo
+//
+//  Created by Tim Lam on 28/7/2017.
+//  Copyright © 2017 Tim Lam. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TestDemo/ViewController.m
@@ -0,0 +1,30 @@
+//
+//  ViewController.m
+//  TestDemo
+//
+//  Created by Tim Lam on 28/7/2017.
+//  Copyright © 2017 Tim Lam. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+    NSLog(@"init commit");
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
